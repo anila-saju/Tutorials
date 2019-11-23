902fad26887f726935e17414677162305aba75de
Lucian Vilcea <a.lucian.vilcea@gmail.com>
2019-11-23 20:25:23 +0200

    Add play sound on click

---
+++ PlayingAudioOnJavaScriptEvent/scripts/script.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    // add event handler for the click event
+    $(".animal").click(function () {
+        // retrieve the data-animal attribute
+        var animal = $(this).attr("data-animal");
+
+        // create an <audio> element
+        var audio = document.createElement("audio");
+
+        // set the source of the <audio> element with
+        // the proper .ogg file
+        $(audio).attr("src", "media/" + animal + ".ogg");
+
+        // play the sound
+        audio.play();
+    });
+});
